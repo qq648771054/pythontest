1429eae4ed46d02a740d3e8d20537dbd335c15e2
wb.zhangshenglong <648771054@qq.com>
2021-02-01 20:16:14 +0800

    重构

--- README.md
+++ README.md
@@ -7,4 +7,5 @@ winpcap 4.1.3
 
 结构:
 spider:爬虫
-socket:抓包
+pcap:抓包
+translation:翻译
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
-import translation.YouDao as YouDao
-import spider.BQKan as BQKan
+from spider import YouDao
+from spider import BQKan
 
 if __name__ == '__main__':
     pass
     # BQKan.downloadAllTexts('0_790/', dirpath=r'C:\Users\wb.zhangshenglong\Desktop\test')
     # print BQKan.downloadText('0_790/36873824.html')
-    # print YouDao.translate(raw_input('翻译内容:'), fromLan='zh-CHS', toLan='ja')
+    print YouDao.translate(raw_input('翻译内容:'), fromLan='zh-CHS', toLan='ja')
