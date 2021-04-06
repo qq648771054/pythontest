e67eb989e37995656e016983fd08190147b493e6
wb.zhangshenglong <648771054@qq.com>
2021-04-06 21:08:37 +0800

    1

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-*-
 from Lib import *
-class myTime(object):
-    @staticmethod
-    def time():
-        return 123
-
-sys.modules['time'] = myTime
+import pytz
 import time
-print(time.time())
+import datetime
+nowTime = datetime.datetime.fromtimestamp(time.time())
+cntz = pytz.timezone('Asia/Shanghai')
+tz = pytz.timezone('America/Los_Angeles')
+currentTime = tz.localize(nowTime).astimezone(cntz)
+print(currentTime, nowTime.astimezone(cntz))
+print(currentTime.timestamp(), nowTime.timestamp())
+'''
+16 17 18 19 20 21 22 23 00 01 02 03 04 05 06
+20 21 22 23 00 01 02 03 04 05 06 07 08 09 10
+'''
