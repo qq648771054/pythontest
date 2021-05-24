04f48749b284b224a58eb8b491df138a8f38119a
wb.zhangshenglong <648771054@qq.com>
2021-05-24 15:11:39 +0800

    1

--- reinforcement-learning/Gobang.py
+++ reinforcement-learning/Gobang.py
@@ -85,39 +85,49 @@ class Agent(object):
     def __init__(self, env, memroySize):
         self.env = env
         self.model = self._createModel(self.env.SIZE)
+        self.temp_exp = []
         self.exp = []
         self.memory = []
+        self.memoryWeight = []
         self.memorySize = memroySize
         self.memoryIter = 0
         self.mcts = MCTS(self.env, self)
 
     def saveExp(self, state, prop, player):
-        self.exp.append((state, prop, player))
+        self.temp_exp.append((state, prop, player))
 
-    def clearExp(self):
+    def saveMemory(self, scale):
+        for m, resultState in self.exp:
+            self.appendMemory(m, scale[resultState])
         self.exp = []
 
-    def appendMemory(self, m):
+    def appendMemory(self, m, weight):
         if len(self.memory) < self.memorySize:
             self.memory.append(m)
+            self.memoryWeight.append(weight)
         else:
             self.memory[self.memoryIter] = m
+            self.memoryWeight[self.memoryIter] = weight
             self.memoryIter = (self.memoryIter + 1) % self.memorySize
+
+    def discountMemoryWeight(self, discount=0.8):
+        for i, w in enumerate(self.memoryWeight):
+            self.memoryWeight[i] = w * discount
 
     def sampleMemory(self, batch):
         if len(self.memory) < batch:
-            idx = np.random.choice(len(self.memory), size=int(len(self.memory) * 0.8))
+            idx = np.random.choice(len(self.memory), size=int(len(self.memory) * 0.8), p=normallize(self.memoryWeight))
         else:
-            idx = np.random.choice(len(self.memory), size=batch)
+            idx = np.random.choice(len(self.memory), size=batch, p=normallize(self.memoryWeight))
         return [[self.memory[i][j] for i in idx] for j in range(3)]
 
-    def setWinner(self, winner):
-        for state, prop, player in self.exp:
+    def setWinner(self, winner, resultState):
+        for state, prop, player in self.temp_exp:
             if winner == 0:
-                self.appendMemory((state, prop, 0.5))
+                self.exp.append(((state, prop, 0.5), resultState))
             else:
-                self.appendMemory((state, prop, 1 if player == winner else 0))
-        self.clearExp()
+                self.exp.append(((state, prop, 1.0 if winner == player else 0.0), resultState))
+        self.temp_exp = []
 
     def chooseAction(self, state, mode=0, mcts=None):
         if mode == 2:
@@ -215,7 +225,7 @@ class Agent(object):
             inputs = tf.keras.Input(shape=(size, size, 1))
             outputs = tf.keras.layers.Conv2D(256, (3, 3), activation='relu', padding='same')(inputs)
             outputs = tf.keras.layers.BatchNormalization()(outputs)
-            outputs = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', padding='same')(outputs)
+            outputs = tf.keras.layers.Conv2D(256, (3, 3), activation='relu')(outputs)
             outputs = tf.keras.layers.BatchNormalization()(outputs)
             outputs = tf.keras.layers.Conv2D(128, (3, 3), activation='relu')(outputs)
             outputs = tf.keras.layers.BatchNormalization()(outputs)
@@ -223,7 +233,7 @@ class Agent(object):
             outputs = tf.keras.layers.Dense(256, activation='relu')(outputs)
             outputs = tf.keras.layers.BatchNormalization()(outputs)
             outputs = tf.keras.layers.Dropout(0.3)(outputs)
-            outputs = tf.keras.layers.Dense(256, activation='relu')(outputs)
+            outputs = tf.keras.layers.Dense(512, activation='relu')(outputs)
             outputs = tf.keras.layers.BatchNormalization()(outputs)
             outputs = tf.keras.layers.Dropout(0.3)(outputs)
             output1 = tf.keras.layers.Dense(size * size, activation='softmax')(outputs)
@@ -261,23 +271,38 @@ class Image(object):
         self.ax.set_xlim(x[0], x[len(x) - 1])
         self.ax.figure.canvas.draw()
         return self.line,
-
-class ThreadGobang(Thread.ThreadBase):
+'''
+showProcess=False, player=0, mode=0,
+                          boardSize=5, boardWin=4,
+                          savePath=getDataFilePath('GoBang_5_4_1'), logName='log.txt'
+'''
+class Gobang(object):
     saveType = {
         'episode': (str, int),
         'winRate': (json.dumps, json.loads),
     }
     bakFrequence = 500
-    def __init__(self, *args, **kwargs):
-        super(ThreadGobang, self).__init__(*args, **kwargs)
+    def __init__(self, agentType,
+                 showProcess=True, player=0, mode=0,
+                 boardSize=15, boardWin=5,
+                 savePath='', logName='log.txt'):
+        self.agentType = agentType
+        self.showProcess = showProcess
+        self.player = player
+        self.mode = mode
+        self.boardSize = boardSize
+        self.boardWin = boardWin
+        self.savePath = savePath
+        self.logName = logName
+        self.env = None
         self.lastClick = None
         if self.player != 0:
             self.showProcess = True
 
     def run(self):
-        trainPeriod = 100
-        memorySize = trainPeriod * 15 * 5
-        batchSize = memorySize // 2
+        trainPeriod = 50
+        memorySize = trainPeriod * 20 * 10
+        batchSize = memorySize // 3
         env = envoriment.Gobang(onGridClick=self.onGridClick)
         env.reshape(self.boardSize, self.boardWin)
         self.env = env
@@ -290,7 +315,7 @@ class ThreadGobang(Thread.ThreadBase):
         else:
             episode = 0
             self.winRate = []
-        self.log(agent.model.summary())
+        agent.model.summary()
         agentPre = self.agentType(env, memorySize)
         agentPre.model = copyModel(agent.model)
         if self.savePath and hasattr(self, 'logName'):
@@ -322,29 +347,41 @@ class ThreadGobang(Thread.ThreadBase):
             episode += 1
             self.log('path {}'.format(path))
             self.log('episode {}, winner {}, step {}, spend {} seconds'.format(episode, winner, step, time.time() - s1))
-            agent.setWinner(winner)
             if winner == 1:
                 win += 1
+                agent.setWinner(winner, 0)
             elif winner == 2:
                 lose += 1
+                agent.setWinner(winner, 1)
             else:
                 draw += 1
+                agent.setWinner(winner, 2)
             if episode % trainPeriod == 0:
                 self.winRate.append([episode, win, lose, draw])
-                win, lose, draw = 0, 0, 0
-                for i in range(5):
+                sampleTypes = 0.0
+                sampleTypes += 1.0 if win > 0 else 0.0
+                sampleTypes += 1.0 if lose > 0 else 0.0
+                sampleTypes += 1.0 if draw > 0 else 0.0
+                std = trainPeriod / sampleTypes
+                winScale = std / win if win > 0 else 1.0
+                loseScale = std / lose if lose > 0 else 1.0
+                drawScale = std / draw if draw > 0 else 1.0
+                agent.saveMemory(scale=[winScale, loseScale, drawScale])
+                for i in range(4):
                     s2 = time.time()
                     agent.learn(batchSize)
                     self.log('learn spend {} seconds'.format(time.time() - s2))
                     s3 = time.time()
-                    rate = self.comapreWithPre(agent, agentPre)
-                    if rate < 0.55:
+                    winRate = self.comapreWithPre(agent, agentPre)
+                    if winRate < 0.6:
                         agent.model = self.save(agentPre.model, episode=episode, winRate=self.winRate)
                         self.log('train result: {}, spend {} seconds'.format(0, time.time() - s3))
                     else:
                         agentPre.model = self.save(agent.model, episode=episode, winRate=self.winRate)
                         self.log('train result: {}, spend {} seconds'.format(1, time.time() - s3))
                         break
+                agent.discountMemoryWeight(0.8)
+                win, lose, draw = 0, 0, 0
 
     def onGridClick(self, x, y):
         self.lastClick = y * self.env.SIZE + x
@@ -394,12 +431,12 @@ class ThreadGobang(Thread.ThreadBase):
                 episode += 1
                 self.log('self battle {}: path {}, expect {}, winner {}'.format(episode, step, agentIdx, winner))
         self.log('self battle win rate: {}'.format(win / (win + lose) if win + lose > 0 else 0))
-        return win / (win + lose) >= 0.55 if win + lose > 0 else 0
+        return win / (win + lose) if win + lose > 0 else 0
 
     def load(self):
         config = None
         if self.savePath:
-            if os.path.exists(self.savePath):
+            if os.path.exists(os.path.join(self.savePath, 'config.txt')):
                 config = self.readConfig(os.path.join(self.savePath, 'config.txt'))
                 modelPath = os.path.join(self.savePath, 'model_{}-{}.h5'.format(
                     (config['episode'] // self.bakFrequence) * self.bakFrequence,
@@ -407,7 +444,7 @@ class ThreadGobang(Thread.ThreadBase):
                 ))
                 model = tf.keras.models.load_model(modelPath)
                 config['model'] = model
-            else:
+            elif not os.path.exists(self.savePath):
                 os.mkdir(self.savePath)
         return config
 
@@ -456,27 +493,21 @@ class ThreadGobang(Thread.ThreadBase):
             self.logs.append(s)
             self.logs.append('\n')
 
+    def render(self, sleepTime=None):
+        if self.showProcess:
+            self.env.render()
+            if sleepTime:
+                time.sleep(sleepTime)
+
 
 if __name__ == '__main__':
-    # thread = ThreadGobang(Agent, showProcess=False, player=1, mode=1,
-    #                       boardSize=3, boardWin=3,
-    #                       savePath=getDataFilePath('GoBang_3_3'), logName='log.txt')
-
-    thread = ThreadGobang(Agent, showProcess=False, player=0, mode=0,
+    game = Gobang(Agent, showProcess=False, player=0, mode=0,
                           boardSize=5, boardWin=4,
-                          savePath=getDataFilePath('GoBang_5_4'), logName='log.txt')
-    thread.start()
-    while True:
-        cmd = input()
-        if cmd == 'process 0':
-            thread.showProcess = False
-        elif cmd == 'process 1':
-            thread.showProcess = True
-        elif cmd == 'image':
-            thread.showWinRate()
-        elif cmd == 'mode 0':
-            thread.mode = 0
-        elif cmd == 'mode 1':
-            thread.mode = 1
+                          savePath=getDataFilePath('GoBang_5_4_1'), logName='log.txt')
+
+    # game = Gobang(Agent, showProcess=False, player=2, mode=1,
+    #                       boardSize=3, boardWin=3,
+    #                       savePath=getDataFilePath('GoBang_3_3_1'), logName='log.txt')
+    game.run()
 
 
